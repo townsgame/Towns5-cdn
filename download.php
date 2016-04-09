b5e0cc5a3dffd85f5de88e295e7e5a5ad56473e6
Pavol Hejný <me@pavolhejny.com>
2016-04-09 12:55:55 +0200

    [PH] downloading files

---
+++ download.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author ©Towns.cz
+ * @fileOverview Functions for rendring trees and rocks
+ */
+//======================================================================================================================
+
+
+
+//todo $_GET['width'];
+
+
+list($uid,$filename) = explode('-',$_GET['file']);
+
+$filename=base64_decode($filename);
+
+
+
+$path=__DIR__.'/storage/'.$_GET['file'];
+
+
+
+
+if(!file_exists($path)){
+
+    //todo
+    echo('wrong file');
+
+}else{
+
+
+    header("Content-Disposition: inline; filename=" . $filename);
+    header("Cache-Control: max-age=".(3600*24*100));
+    header('Content-Type: '.mime_content_type($path));
+    readfile($path);
+
+
+}
+
+
+
+
+/**/

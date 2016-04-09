d39023ae6026aca45aae4b857d85ae25f2d2e2b5
Pavol Hejný <me@pavolhejny.com>
2016-04-09 15:08:00 +0200

    [PH] CORS

--- upload.php
+++ upload.php
@@ -16,35 +16,41 @@ $file_accepted_types=array(
     //todo maybe bmp?
 
 );
-$file_max_size='15MB';
+$file_max_size='7MB';
+
+//----------
+
+$file_max_size=files\toByteSize($file_max_size);
+$all_files_max_size=$file_max_size*count($_FILES)+1000;
 
 
-ini_set('post_max_size', $file_max_size);
-ini_set('upload_max_filesize', $file_max_size);
+ini_set('post_max_size', $all_files_max_size);
+ini_set('upload_max_filesize', $all_files_max_size);
 
 
 
 $response=array();
 $response_all_ok=true;
 
-
+//todo check and than move files
 foreach($_FILES as $key=>$file){
 
+    //print_r($file);
 
-    if($file['size']>files\toByteSize($file_max_size)){
+    if($file['size']>$file_max_size){
         //-----------------------------------------------------------------Size error
 
         $response[$key]='Error: extended max type';
         $response_all_ok=false;
 
-        //-----------------------------------------------------------------
-    }elseif(in_array($file['type'],$file_accepted_types)===false){
+        //-----------------------------------------------------------------*/
+    /**/}elseif(in_array($file['type'],$file_accepted_types)===false){
         //-----------------------------------------------------------------Type error
 
-        $response[$key]='Error: wrong file type';
+        $response[$key]='Error: wrong file type '.$file['type'];
         $response_all_ok=false;
 
-        //-----------------------------------------------------------------
+        //-----------------------------------------------------------------*/
     }else{
         //-----------------------------------------------------------------Success
 

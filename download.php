84ed6d19a7eec7a370fafb878be5d61fea3c040b
Pavol Hejný <me@pavolhejny.com>
2016-04-11 20:20:34 +0200

    [PH] download image width

--- download.php
+++ download.php
@@ -28,10 +28,33 @@ if(!file_exists($path)){
 }else{
 
 
+    //todo widths array
+
+    $width=intval($_GET['width']);
+
+    $cache_path=files\cacheFile(array($path,$width),'dat','images');
+
+    if(!file_exists($cache_path) or isset($_GET['notmp']) or filesize($cache_path)<10 /** or 1/**/) {
+        //_________________________________________
+
+        $src = imagecreatefromstring(file_get_contents($path));
+        $dest = graphic\imgresizew($src, $width);
+
+
+
+        imagesavealpha($dest, true);
+        imagepng($dest,$cache_path);
+
+        //_________________________________________
+    }
+
+
+
+
     header("Content-Disposition: inline; filename=" . $filename);
     header("Cache-Control: max-age=".(3600*24*100));
     header('Content-Type: '.mime_content_type($path));
-    readfile($path);
+    readfile($cache_path);
 
 
 }

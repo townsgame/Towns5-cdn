df22118aac775fb001f5ffef127c6c9952e86507
Stefan Kecskes <mr.kecskes@gmail.com>
2016-04-14 03:03:28 +0100

    fix graphic name for  autoloader

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 require_once(__DIR__ . '/app/Init.php');
 //require_once(__DIR__ . '/app/Files.php');
-//require_once(__DIR__.' /app/graphic.app.php');
+//require_once(__DIR__.' /app/Graphic.php');
 
 $controller = new HomeController();
 $controller->response();
